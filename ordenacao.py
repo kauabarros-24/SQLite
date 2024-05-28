db1db03a5e307967135b58c2c0dcdc6aa4f2b060
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-05-28 15:01:06 -0300

    Consulta do tempo médio de tarefas demoradas

--- ordenacao.py
+++ ordenacao.py
@@ -4,9 +4,9 @@ import sqlite3
 conexao = sqlite3.connect('neps_sql_course.db')
 cursor = conexao.cursor()
 
-cursor.execute('SELECT title, difficulty FROM programming_task ORDER BY difficulty')
-for c in cursor.fetchall():
-    print(c)
+cursor.execute('SELECT programming_task_id, AVG(runtime) AS avg_media_time FROM submission GROUP BY programming_task_id HAVING avg_media_time > 0.1')
 
+for c in cursor.fetchall():
+    print(c[0], "id -", f"{c[1]}s")
 cursor.close()
 conexao.close()
