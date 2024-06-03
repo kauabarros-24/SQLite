db1d19c07585a9dd319512b7a462d038888c96b1
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-06-03 15:37:48 -0300

    Ordenação simples no banco de ados

--- consulta.py
+++ consulta.py
@@ -1,22 +1,12 @@
 import sqlite3
 
-#Conexao
-conexao = sqlite3.connect("db.db")
+conexao = sqlite3.connect('neps_sql_course.db')
 cursor = conexao.cursor()
 
-#cursor.execute("SELECT nome FROM vendedores")
-#nomeVendedores = cursor.fetchall()
-#for vendedores in nomeVendedores:
-#    print(vendedores)
+cursor.execute('SELECT title, difficulty FROM programming_task ORDER BY difficulty')
 
-cursor.execute("SELECT nome FROM vendedores WHERE id = 1")
-
-user1 = cursor.fetchone()
-
-if user1:
-    print(user1)
-else:
-    print("Usuário não encontrado")
+for c in cursor.fetchall():
+    print(c)
 
 cursor.close()
-conexao.close
+conexao.close()
