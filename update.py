6186fac58205d00a7a20deb7a8a50239b8ddf6e1
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-06-07 11:00:26 -0300

    Delete com WHERE

--- update.py
+++ update.py
@@ -3,10 +3,9 @@ import sqlite3
 conexao = sqlite3.connect('neps_sql_course.db')
 cursor = conexao.cursor()
 
-new_username = 'new_username'
 user_id = 1
 
-cursor.execute("""UPDATE user SET username = ? WHERE id = ?""", (new_username, user_id))
+cursor.execute('DELETE FROM user WHERE id = ?', (user_id,))
 
 conexao.commit()
 
