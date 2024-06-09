884433f11f351fa8427574b61c08d472a19cda8d
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-06-09 16:09:13 -0300

    Join

--- join.py
+++ join.py
@@ -3,4 +3,9 @@ import sqlite3
 conexao = sqlite3.connect('neps_sql_course.db')
 cursor = conexao.cursor()
 
-cursor.execute('SELECT user.username, submission.solution FROM user')
+cursor.execute('SELECT username FROM user UNION SELECT name FROM achievement')
+for c in cursor.fetchall():
+    print(c[0])
+
+cursor.close()
+conexao.close()
