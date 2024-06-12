712961602e05d8ad38435b8449d779490e122fad
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-06-12 14:51:42 -0300

    INNER JOIN da nome de usuário com o id da solução resolvida pelo mesmo

--- join.py
+++ join.py
@@ -3,7 +3,11 @@ import sqlite3
 conexao = sqlite3.connect('neps_sql_course.db')
 cursor = conexao.cursor()
 
-cursor.execute('SELECT programming_task.id, submission.programming_task_id FROM programming_task INNER JOIN submission ON programming_task.id = submission.id')
+cursor.execute("""
+   SELECT user.username, submission.solution
+    FROM user
+    INNER JOIN submission ON user.id = submission.user_id
+""")
 for c in cursor.fetchall():
     print(c)
 
