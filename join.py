8d1bb9d3a75ad96ee43c52e958d9e40f514b84b9
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-06-12 14:42:50 -0300

    feat: INNER JOIN da chave estrangeira do das tarefas de programação com o próprio id

--- join.py
+++ join.py
@@ -3,9 +3,9 @@ import sqlite3
 conexao = sqlite3.connect('neps_sql_course.db')
 cursor = conexao.cursor()
 
-cursor.execute('SELECT username FROM user UNION SELECT name FROM achievement')
+cursor.execute('SELECT programming_task.id, submission.programming_task_id FROM programming_task INNER JOIN submission ON programming_task.id = submission.id')
 for c in cursor.fetchall():
-    print(c[0])
+    print(c)
 
 cursor.close()
 conexao.close()
--- listagemPelaLinguagem.py
+++ listagemPelaLinguagem.py
@@ -3,6 +3,6 @@ import sqlite3
 con = sqlite3.connect("neps_sql_course.db")
 cur = con.cursor()
 
-language = input("Digite o nome da linguagem de programação: ")
+cur.execute('SELECT programming_task.id, submission.programming_task_id FROM programming_task INNER JOIN submission ON programming_task.id = submission.id')
 
-cur.execute('SELECT ')
+cursor.
